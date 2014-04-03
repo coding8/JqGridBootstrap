fb7a6a4d80de0570775511146dad0f79ea7f1857
O <Coding@yeah.net>
2014-04-03 17:41:48 +0800

    zero

---
+++ JqGridBootstrap/App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace JqGridBootstrap
+{
+    public class BundleConfig
+    {
+        // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
+            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            //jqGrid
+            bundles.Add(new StyleBundle("~/Content/jqGridCSS").Include(
+                     "~/Content/jqGrid/ui.jqgrid.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jqGridJS").Include(
+                     "~/Scripts/jqGrid/jquery.jqGrid.src.js",
+                     "~/Scripts/jqGrid/grid.locale-cn.js"));
+
+        }
+    }
+}
---
+++ JqGridBootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JqGridBootstrap.Startup))]
+namespace JqGridBootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
